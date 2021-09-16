9e3c6746aadd0cd80c93da4a59e125794dc08411
Chethana Manchikatla <chethanamanchikatla@Chethanas-MacBook-Air.local>
2021-09-16 12:35:50 +0530

    added few more methods and surefire plugin

--- src/main/java/io/mc/MathUtils.java
+++ src/main/java/io/mc/MathUtils.java
@@ -1,8 +1,24 @@
 package io.mc;
 
+import java.awt.geom.PathIterator;
+
 public class MathUtils {
-	public int add(int a,int b) {
-		return a+b;
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	public int divide(int a, int b) {
+		return a / b;
+	}
+	
+	
+	public double computeCircleArea(double radius) {
+		return Math.PI * radius * radius;
 	}
 
 }
--- src/test/java/io/mc/MathUtilsTest.java
+++ src/test/java/io/mc/MathUtilsTest.java
@@ -7,8 +7,23 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
-		System.out.println("test passed!");
+	void testAdd() {
+		MathUtils mathutils = new MathUtils();
+		int expected = 2;
+		int actual = mathutils.add(1,1);
+		assertEquals(expected,actual,"add method failed");
+		
+	}
+	
+	@Test
+	void testDivide() {
+		MathUtils mathutils = new MathUtils();
+		assertThrows(NullPointerException.class,()->mathutils.divide(1, 0),"Divide by zero " );
+	}
+	@Test
+	void testcomputeCircleArea() {
+		MathUtils mathutils = new MathUtils();
+		assertEquals(314.1592653589793, mathutils.computeCircleArea(10),"computeCirleArea method failed!");
 	}
 
 }
