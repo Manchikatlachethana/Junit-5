967ea06e405ee1f22949806c4fc3eea27294833b
Chethana Manchikatla <chethanamanchikatla@Chethanas-MacBook-Air.local>
2021-09-17 15:15:46 +0530

    added few more assertions

--- src/test/java/io/mc/MathUtilsTest.java
+++ src/test/java/io/mc/MathUtilsTest.java
@@ -1,29 +1,75 @@
 package io.mc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 class MathUtilsTest {
+	MathUtils mathutils;
+	
+	@BeforeAll
+	static void beforeAllInit() {
+		System.out.println("Before all implementation ");
+	}
+	
+	@BeforeEach
+	void init() {
+		mathutils = new MathUtils();
+	}
+	
+	@AfterEach
+	void cleanup() {
+		System.out.println("cleaning up...");
+	}
+	
 
 	@Test
+	@DisplayName("Testing add method")
 	void testAdd() {
-		MathUtils mathutils = new MathUtils();
 		int expected = 2;
 		int actual = mathutils.add(1,1);
-		assertEquals(expected,actual,"add method failed");
-		
+		assertEquals(expected,actual,"add method failed");	
 	}
 	
 	@Test
+	@DisplayName("Testing multiply method")
+	void testMultiply() {
+		assertAll(
+				() -> assertEquals(16,mathutils.multiply(4, 4)),
+				() -> assertEquals(-2,mathutils.multiply(2, -1)),
+				() -> assertEquals(0,mathutils.multiply(0, 4))
+				);
+	}
+	
+	@Test
+	@EnabledOnOs(OS.MAC)
 	void testDivide() {
-		MathUtils mathutils = new MathUtils();
-		assertThrows(NullPointerException.class,()->mathutils.divide(1, 0),"Divide by zero " );
+	 /*   boolean isServerUp = false;
+		  assumeTrue(isServerUp);    */
+		assertThrows(ArithmeticException.class,()->mathutils.divide(1, 0),"Divide by zero " );
 	}
 	@Test
 	void testcomputeCircleArea() {
-		MathUtils mathutils = new MathUtils();
 		assertEquals(314.1592653589793, mathutils.computeCircleArea(10),"computeCirleArea method failed!");
 	}
+	
+	@Test
+	@Disabled
+	@DisplayName("TDD method should not run")
+	void testDisabled() {
+		fail("This test should be disabled");
+	}
+	
+	
+	
+	
 
 }
